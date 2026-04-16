27ab1f588016184f345a4b777236c04adba589f0
lorenztheuer <lorenztheuer@me.com>
2026-04-16 13:31:16 +0200

    flavor issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 import 'screens/startup_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -9,9 +10,27 @@ import 'firebase_options.dart';
 /// Initialisiert Firebase und startet die Hauptanwendung.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } else {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        if (Firebase.apps.isEmpty) {
+          await Firebase.initializeApp();
+        }
+        break;
+      default:
+        await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        );
+        break;
+    }
+  }
+
   runApp(const MainApp());
 }
 
